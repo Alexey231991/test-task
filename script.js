56b7b13a76eaed0f680032d9c60f4a7f15bff929
Alexey231991 <lorketalex@ukr.net>
2021-04-16 21:44:16 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const headerPosition = $('.header-secondary').offset().top;
+const firstScreenHeight = $('.first-screen-animation').height();
+
+$(window).scroll(function() {
+    const scrollTop = $(this).scrollTop();
+
+    if ( scrollTop > headerPosition ) {
+        $('.header-secondary').addClass('fixed')
+        $('.about').addClass('fixed-header')
+    } else {
+        $('.header-secondary').removeClass('fixed')
+        $('.about').removeClass('fixed-header')
+    }
+
+    if ( scrollTop < firstScreenHeight ) {
+        const percent = scrollTop / (firstScreenHeight / 100);
+
+        const scale = (1 + (percent / 100) * 5).toFixed(3);
+        $('.circles').css({
+            'transform': `scale(${scale})`,
+        })
+    }
+})
+
+
+$('.burger').click(function () {
+    $('.header-secondary .list-menu').toggleClass('active')
+})
